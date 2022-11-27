7b1744cac7e3652b2afe8ba09debce231d8f7a5f
Anthony Sottile <asottile@umich.edu>
2022-11-27 17:38:38 -0500

    day 1 part 1

---
+++ day01/part1.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    n = 0
+    for c in s.strip():
+        if c == '(':
+            n += 1
+        elif c == ')':
+            n -= 1
+        else:
+            raise AssertionError(f'unexpected: {c!r}')
+    return n
+
+
+INPUT_S = '''\
+))(((((
+'''
+EXPECTED = 3
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
