04a9c51a081a7b316af2c8db212e99666030bd65
Anthony Sottile <asottile@umich.edu>
2024-10-07 18:43:48 -0400

    day 4 part 1

---
+++ day04/part1.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import argparse
+import hashlib
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    base = s.strip()
+    i = 1
+    while True:
+        if hashlib.md5(f'{base}{i}'.encode()).hexdigest().startswith('00000'):
+            return i
+        else:
+            i += 1
+
+
+INPUT_S = '''\
+abcdef
+'''
+EXPECTED = 609043
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
