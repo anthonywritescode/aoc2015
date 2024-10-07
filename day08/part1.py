d0355713a9b01e1ac6fcc82f3cc0d702c976ed18
Anthony Sottile <asottile@umich.edu>
2024-10-07 19:56:36 -0400

    day 8 part 1

---
+++ day08/part1.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import argparse
+import ast
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    code = 0
+    data = 0
+    for line in s.splitlines():
+        code += len(line)
+        data += len(ast.literal_eval(line))
+    return code - data
+
+
+INPUT_S = '''\
+""
+"abc"
+"aaa\\"aaa"
+"\\x27"
+'''
+EXPECTED = 12
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
